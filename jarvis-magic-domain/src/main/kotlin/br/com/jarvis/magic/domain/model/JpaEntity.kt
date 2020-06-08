3711bb86112641c471f06b5ec35d1c9624c4f515
SantosVictor1 <victor.udi10@hotmail.com>
2020-06-08 16:34:17 -0300

    Created machine and machine card entity and migration files

--- jarvis-magic-domain/src/main/kotlin/br/com/jarvis/magic/domain/model/JpaEntity.kt
+++ jarvis-magic-domain/src/main/kotlin/br/com/jarvis/magic/domain/model/JpaEntity.kt
@@ -1,7 +1,9 @@
 package br.com.jarvis.magic.domain.model
 
+import org.hibernate.annotations.GenericGenerator
 import java.util.UUID
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
 import javax.persistence.Id
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
@@ -11,8 +13,21 @@ import javax.persistence.Table
 @Table(name = "player")
 data class PlayerEntity (
     @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
     val id: String = UUID.randomUUID().toString(),
     val nickname: String = "",
+    val life: Int = 20,
+    val mana: Int = 20
+)
+
+@Entity
+@Table(name = "machine")
+data class MachineEntity(
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    val id: String = UUID.randomUUID().toString(),
     val life: Int = 20,
     val mana: Int = 20
 )
@@ -21,13 +36,32 @@ data class PlayerEntity (
 @Table(name = "player_card")
 data class PlayerCardEntity(
     @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
     val id: String = UUID.randomUUID().toString(),
     val title: String = "",
     val description: String = "",
     val cost: Int = 0,
     val lifeDamage: Int = 0,
-    val passive: Int = 0,
+    val passive: Int? = null,
     @ManyToOne(targetEntity = PlayerEntity::class)
     @JoinColumn(name = "player_id", nullable = true)
     private val player: PlayerEntity? = null
+)
+
+@Entity
+@Table(name = "machine_card")
+data class MachineCardEntity(
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    val id: String = UUID.randomUUID().toString(),
+    val title: String = "",
+    val description: String = "",
+    val cost: Int = 0,
+    val lifeDamage: Int = 0,
+    val manaDamage: Int? = null,
+    @ManyToOne(targetEntity = MachineEntity::class)
+    @JoinColumn(name = "machine_id", nullable = true)
+    private val machine: MachineEntity? = null
 )
